8a7f0778dc2d7a59c649e4c914b39e3dbba1ff8d
EsteArgen <101402836+EsteArgen@users.noreply.github.com>
2024-03-16 12:29:46 -0500

    Update Zona.java

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -1,55 +1,39 @@
 package gestion;
 
 import java.util.ArrayList;
-
+import java.util.List;
 import zooAnimales.Animal;
 
 public class Zona {
+	
 	private String nombre;
 	private Zoologico zoo;
-	private ArrayList<Animal>animales= new ArrayList<>();
+	private List<Animal> animales = new ArrayList<Animal>();;
+	private Animal[] animal;
 	
 	public Zona() {
-		this(null,null);
 	}
 	
-	public Zona(String nombre,Zoologico zoo) {
-		this.nombre=nombre;
-		this.zoo=zoo;
-		
+	public Zona (String nombre1, Zoologico zoo1) {
+        this.nombre = nombre1;
+        this.zoo = zoo1;
 	}
 	
-	
-	public void setNombre(String nombre) {
-		this.nombre=nombre;
-	}
-	public String getNombre() {
-		return nombre;
+	public void agregarAnimales(Animal NuevoAnimal) {
+        animales.add(NuevoAnimal);
 	}
 	
-	public void setZoo(Zoologico zoo) {
-		this.zoo=zoo;
-	}
-	public Zoologico getZoo() {
-		return zoo;
+	public int cantidadAnimales() {
+		return animales.size();
 	}
 	
-	public void setAnimales(ArrayList<Animal> animales) {
-		this.animales=animales;
-	}
-	public  ArrayList<Animal> getAnimales() {
+	public List<Animal> getAnimales() {
 		return animales;
-	}
-	
-	
-    public void agregarAnimales(Animal animal) {
-    	animales.add(animal);
-    	
-    }
-    public int cantidadAnimales() {
-    	int cantAnimales=animales.size();
-    	return cantAnimales;
-    	
-    }
-
+		}
+	  public String getNombre(){
+		  return nombre;
+		  }
+	  public Zoologico getZoo(){
+		  return zoo;
+		  } 
 }
