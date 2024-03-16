b18547c5179f44bd22e206fb9633ca3f91887143
EsteArgen <101402836+EsteArgen@users.noreply.github.com>
2024-03-16 12:30:11 -0500

    Update Zoologico.java

--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -1,55 +1,40 @@
 package gestion;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class Zoologico {
+	
 	private String nombre;
 	private String ubicacion;
-	private ArrayList<Zona> zonas=new ArrayList<>();
+	private List<Zona> zonass = new ArrayList<Zona>();
+	Zona[] zonas;
 	
-	public Zoologico() {
-		this(null,null);
-		
-	}
-	
-	public Zoologico(String nombre, String ubicacion) {
-		this.nombre=nombre;
-		this.ubicacion=ubicacion;
-		
-	}
-	
-	public void setNombre(String nombre) {
-		this.nombre=nombre;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-     
-	public void setUbicacion(String ubicacion) {
-		this.ubicacion=ubicacion;
-	}
-	public String getUbicacion() {
-		return ubicacion;
-	}
-	
-	public void setZonas(ArrayList<Zona> zonas) {
-		this.zonas=zonas;
-	}
-	public ArrayList<Zona> getZona() {
-		return zonas;
-	}
-	
-	public void agregarZonas(Zona zona) {
-		zonas.add(zona);
-		
-	}
-	
-	public int cantidadTotalAnimales() {
-		int totalAnimales=0;
-		for (Zona zonas : zonas) {
-			totalAnimales+=zonas.cantidadAnimales();
-				
-		}
-		return totalAnimales;
-	}
+	public Zoologico(){}
 
+	public Zoologico(String nombre1, String ubicacion1) {
+        this.nombre = nombre1;
+        this.ubicacion = ubicacion1;
+        this.zonas = new Zona[10];
+        
+	}
+        
+        public void agregarZonas(Zona NuevaZona) {
+            zonass.add(NuevaZona); 
+	}
+        
+        public int cantidadTotalAnimales(){
+            int total=0;
+            for(int i=0; i<zonass.size(); i++){total+=zonass.get(i).cantidadAnimales();}
+            return total;}
+        
+        public String getNombre(){
+        	return nombre;
+        	}
+        public String getUbicacion(){
+        	return ubicacion;
+        }
+        public List<Zona> getZona(){
+        	return zonass;
+        	}
 }
