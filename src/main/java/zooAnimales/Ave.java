7c952df8a0629b55dd558571565c5e1b4f67bec6
EsteArgen <101402836+EsteArgen@users.noreply.github.com>
2024-03-16 12:31:42 -0500

    Update Ave.java

--- src/main/java/zooAnimales/Ave.java
+++ src/main/java/zooAnimales/Ave.java
@@ -1,56 +1,43 @@
 package zooAnimales;
-import java.util.ArrayList;
 
-public class Ave extends Animal{
-	private static ArrayList<Ave> listado=new ArrayList<>();
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ave extends Animal {
+	
+	private static List<Ave> lista = new ArrayList<Ave>();
+	private static Ave[] listado;
 	public static int halcones;
 	public static int aguilas;
 	private String colorPlumas;
 	
-	public Ave() {
-		this(null,0,null,null,null);
-	}
+	public Ave(){
+		totalAnimales++;
+		lista.add(this);
+		}
+	public Ave(String nombre1,int edad1, String habitat1,String genero1, String colorPlumas1){
+	  super.setNombre(nombre1);
+	  super.setEdad(edad1);
+	  super.setHabitat(habitat1);
+	  super.setGenero(genero1);
+	  this.colorPlumas = colorPlumas1;
+	  lista.add(this);
+	  totalAnimales++;
+	  }
 	
-	public Ave(String nombre, int edad, String habitat, String genero, String colorPlumas) {
-		super(nombre,edad,habitat,genero);
-		this.colorPlumas=colorPlumas;
-		Ave.listado.add(this);
-	}
-	
-	
-	public static void setListado(ArrayList<Ave> listado) {
-		Ave.listado=listado;
-	}
-	public static ArrayList<Ave> getListado() {
-		return listado;
-	}
-	
-	public void setColorPlumas(String colorPlumas) {
-		this.colorPlumas=colorPlumas;
-	}
-	public String getColorPlumas() {
-		return colorPlumas;
-	}
-	
-	public int cantidadAves() {
-		return Ave.listado.size();
-		
-	}
-	
-	public String movimiento() {
-		return "volar";
-	}
-	
-	public static Ave crearHalcon(String nombre, int edad, String genero) {
-		Ave Ave=new Ave(nombre,edad, "montanas", genero, "cafe glorioso");
-		halcones++;
-		return Ave;
-	}
-	
-	public static Ave crearAguila(String nombre, int edad, String genero) {
-		Ave Ave=new Ave(nombre,edad, "montanas", genero, "blanco y amarillo");
-		aguilas++;
-		return Ave;
-	}
+	public static int cantidadAves(){
+		return lista.size();
+		}
+	public static Ave crearHalcon(String Nnombre,int Nedad, String Ngenero){
+	    halcones++;
+	    return new Ave(Nnombre,Nedad,"montanas",Ngenero, "cafe glorioso");
+	  }
+	 public static Ave crearAguila(String Nnombre,int Nedad, String Ngenero){
+	    aguilas++;
+	    return new Ave(Nnombre,Nedad,"montanas",Ngenero, "blanco y amarillo");
+	  }
+	 public String getColorPlumas(){
+		 return colorPlumas;
+		 }
 
 }
