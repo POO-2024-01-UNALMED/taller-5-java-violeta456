7d876dc0d95d6bc2dd96baa69cf065f3ae2d18e1
EsteArgen <101402836+EsteArgen@users.noreply.github.com>
2024-03-16 12:27:08 -0500

    Update Zona.java

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -9,33 +9,47 @@ public class Zona {
 	private Zoologico zoo;
 	private ArrayList<Animal>animales= new ArrayList<>();
 	
-	public Zona(String nombre, Zoologico zoo) {
+	public Zona() {
+		this(null,null);
+	}
+	
+	public Zona(String nombre,Zoologico zoo) {
 		this.nombre=nombre;
 		this.zoo=zoo;
-	}
-	public Zona() {}
-	
-	public void agregarAnimales(Animal animal) {
-		animales.add(animal);
-	}
-	public int cantidadAnimales() {
-		return animales.size();
+		
 	}
 	
 	
-	public Zoologico getZoo() {
-		return this.zoo;
-	}
-	public void setZoo(Zoologico zoo) {
-		this.zoo=zoo;
-	}
-	public String getNombre() {
-		return this.nombre;
-	}
 	public void setNombre(String nombre) {
 		this.nombre=nombre;
 	}
-
-}
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setZoo(Zoologico zoo) {
+		this.zoo=zoo;
+	}
+	public Zoologico getZoo() {
+		return zoo;
+	}
+	
+	public void setAnimales(ArrayList<Animal> animales) {
+		this.animales=animales;
+	}
+	public  ArrayList<Animal> getAnimales() {
+		return animales;
+	}
+	
+	
+    public void agregarAnimales(Animal animal) {
+    	animales.add(animal);
+    	
+    }
+    public int cantidadAnimales() {
+    	int cantAnimales=animales.size();
+    	return cantAnimales;
+    	
+    }
 
 }
