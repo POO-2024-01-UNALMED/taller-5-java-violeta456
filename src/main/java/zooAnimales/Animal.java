06d49385c0b330f2edc73540a292d7736c003315
EsteArgen <101402836+EsteArgen@users.noreply.github.com>
2024-03-16 12:31:20 -0500

    Update Animal.java

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -3,98 +3,61 @@ package zooAnimales;
 import gestion.Zona;
 
 public class Animal {
-	private static int totalAnimales;
+	
+	int totalAnimales = 0;
 	private String nombre;
 	private int edad;
 	private String habitat;
 	private String genero;
-	private Zona zona;
+	private Zona[] zona;
 	
-	public Animal() {
-		this(null,0,null,null);
-	}
-	
-	public Animal( String nombre, int edad, String habitat, String genero) {
-		this.nombre=nombre;
-		this.edad=edad;
-		this.habitat=habitat;
-		this.genero=genero;
-		this.zona=null;
+	public Animal(){
 		totalAnimales++;
+		}
 	
-	}
-	
-	
-	public static void setTotalAnimales(int totalAnimales) {
-		Animal.totalAnimales=totalAnimales;
-	}
-	public static int getTotalAnimales() {
-		return totalAnimales;
-	}
-	
-	public void setNombre(String nombre) {
-		this.nombre=nombre;
-	}
-	public String getNombre() {
-		return nombre;
+	public Animal(String nombre, int edad, String habitat, String genero) {
+	    this.nombre = nombre;
+	    this.edad = edad;
+	    this.habitat = habitat;
+	    this.genero = genero;
+	    totalAnimales++;
 	}
 	
-	public void setEdad(int edad) {
-		this.edad=edad;
-	}
-	public int getEdad() {
-		return edad;
-	}
+	public String movimiento(){
+	    if(this instanceof Anfibio){return "saltar";}
+	    else if(this instanceof Ave){return "volar";}
+	    else if(this instanceof Reptil){return "reptar";}
+	    else if(this instanceof Pez){return "nadar";}
+	    else return "desplazarse";
+	  }
 	
-	public void setHabitat(String habitat) {
-		this.habitat=habitat;
-	}
+	public static String totalPorTipo(){return "Mamiferos: "+Mamifero.cantidadMamiferos()+"\n" + "Aves: "+Ave.cantidadAves()+"\n" + "Reptiles: "+Reptil.cantidadReptiles()+"\n" + "Peces: "+Pez.cantidadPeces()+"\n" + "Anfibios: "+Anfibio.cantidadAnfibios();}
+	
+	
+	public String toString(){
+		return "Mi nombre es "+this.nombre+", tengo una edad de " +this.edad+", habito en "+this.habitat+" y mi genero es "+this.genero;
+		}
+	
+	  public String getGenero() {
+		  return this.genero;
+		  }
+	  public void setGenero(String genero1) {
+		  this.genero=genero1;
+		  }
 	public String getHabitat() {
-		return habitat;
-	}
-	
-	public void setGenero(String genero) {
-		this.genero=genero;
-	}
-	public String getGenero() {
-		return genero;
-	}
-	
-	public void setZona(Zona zona) {
-		this.zona=zona;
-	}
-	public Zona getZona() {
-		return zona;
-	}
-	
-	
-	public String movimiento() {
-		return "desplazarse";
-	}
-	
-	public static String totalPorTipo() {
-		
-		return "Mamiferos: "+Mamifero.getListado().size()+"\n"+ 
-		"Aves: "+Ave.getListado().size()+"\n"+ 
-		"Reptiles: "+Reptil.getListado().size()+"\n" + 
-		"Peces: " +Pez.getListado().size()+"\n"+ 
-		"Anfibios: "+Anfibio.getListado().size();
-		
-		
-	}
-	
-	public String toString() {
-		if (getZona()!=null) {
-			return "Mi nombre es "+ getNombre()+", tengo una edad de " +getEdad()+ ", habito en "+getHabitat()+" y mi genero es "+getGenero()+", la zona en la que me ubico es "+getZona().getNombre()+", en el "+getZona().getZoo().getNombre();
+		return this.habitat;
 		}
-		else {
-		    return "Mi nombre es "+ getNombre()+", tengo una edad de " +getEdad()+ ", habito en "+getHabitat()+" y mi genero es "+getGenero();
-		}
-	}
+	  public void setHabitat(String habitat1) {
+		  this.habitat=habitat1;}
+	  public String getNombre() {return this.nombre;
+	  }
+	  public void setNombre(String nombre1) {
+		  this.nombre=nombre1;
+		  }
+	  public int getEdad() {
+		  return this.edad;}
+	  public void setEdad(int edad1) {
+		  this.edad=edad1;
+	  }
 
-
-}
-	public void setGenero(String genero) {
-		this.genero=genero;
-	}
 }
